1a304f1d4816258fcd6a05c8366361e5a61b020c
sujaybk2 <sjbhaskar2@gmail.com>
2022-08-13 12:29:52 -0700

    Adding comments

--- src/main/java/PosORNegative.java
+++ src/main/java/PosORNegative.java
@@ -1,8 +1,10 @@
+//This Program is to check if the number is positive or negative
 class PosORNegative
 {
     public static void main(String[] args)
     {
     int no=34;
+        //below if the condition checks if the number is greater than zero, and returns as a positive number if condition is true
     if(no>0)
     {
     System.out.println("pos");
