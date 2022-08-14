fd1b39765df2c7a678630547189605f67e6f175d
sujaybk2 <sjbhaskar2@gmail.com>
2022-08-14 15:35:42 -0700

    Adding scanner to "NumbersAndItsSquare"

--- src/main/java/NumberanditsSquare.java
+++ src/main/java/NumberanditsSquare.java
@@ -3,11 +3,13 @@ public class NumberanditsSquare {
     public static void main(String[] args)
     {
         float i;
+        //The above declaration can also be "int 1" should you wish to make it a whole number
         Scanner x=new Scanner(System.in);
         System.out.println("Enter a number that needs to be squared : ");
         i=x.nextFloat();
         System.out.println(i);
         System.out.println(i*i);
+
     }
 
 }
