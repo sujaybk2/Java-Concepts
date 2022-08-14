d10b9e86e794d6b5e3336fab767c1808a9f7c0c8
sujaybk2 <sjbhaskar2@gmail.com>
2022-08-13 17:07:41 -0700

    Entered scan code for "NumberanditsSquare"

--- src/main/java/NumberanditsSquare.java
+++ src/main/java/NumberanditsSquare.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 public class NumberanditsSquare {
     public static void main(String[] args)
     {
-        int i=4, j;
-        System.out.Scanln
+        int i=4;
+        System.out.println("Enter a number that needs to be squared : ");
+        Scanner j=new Scanner(System.in);
+        System.out.println(j);
         System.out.println(i*i);
     }
 
