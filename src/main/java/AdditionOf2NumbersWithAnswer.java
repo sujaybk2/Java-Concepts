4e48e3719d33d48b426f91814a20a59e22a74284
sujaybk2 <sjbhaskar2@gmail.com>
2022-08-14 10:37:31 -0700

    Trying from IntelJ to commit

---
+++ src/main/java/AdditionOf2NumbersWithAnswer.java
@@ -0,0 +1,20 @@
+//This program is to get 2 numbers and perform addition of 2 numbers and display Addition Answer
+
+import java.util.Scanner;
+
+public class AdditionOf2NumbersWithAnswer {
+    public static void main(String[] args)
+    {
+//        int a,b;
+//        Scanner x=new Scanner(System.in);
+//        System.out.println("Enter the value of a");
+//        a = x.nextInt();
+//        System.out.println("Enter the value of b");
+//        b = x.nextInt();
+//        System.out.println("The value assigned to x is: " +x.nextLine());
+//        System.out.println("The value assigned to a and b is: "+ (a + b));
+
+
+
+    }
+}
