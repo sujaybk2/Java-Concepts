20467333537274f39e8c221b904b6ea74bdb2bab
sujaybk2 <sjbhaskar2@gmail.com>
2022-08-14 19:38:30 -0700

    Updated the syntax

--- src/main/java/PosORNegative.java
+++ src/main/java/PosORNegative.java
@@ -7,26 +7,36 @@ import java.util.Scanner;
 {
     public static void main(String[] args)
     {
-    int no=0, j;
+    int no=-1,j;
+    //String a="abc";
+    //char s='a';
         //below the scanner will initialize for j
         Scanner objforj = new Scanner(System.in);
-        j=objforj.nextInt();
+        System.out.println("Enter your number");
+        j = objforj.nextInt();
         //below if the condition checks if the no variable number is greater than zero, and returns as a positive number if condition is true
             if(no>=0 )
             {
-            System.out.println("No variable is positive");
+            System.out.println("The no initialized variable by Sujay in the code is positive");
             }
             else if(no<0)
             {
-            System.out.println("No variable is negative");
+            System.out.println("The no initialized variable by Sujay in the code is negative");
             }
 
             //below condition is for j
-        if(j>=0) {
+        if(j>0) {
             System.out.println("You entered a Positive Number");
-        } else if(j<0) {
+                }
+        else if(j<0)
+                {
             System.out.println("You entered a negative number");
-        }
+                }
+        else
+                {
+            System.out.println("Try something else");
+                }
+
 
     }
 }
