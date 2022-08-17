d6276ffcf40101b6089b393d7a48091cbb644799
sujaybk2 <sjbhaskar2@gmail.com>
2022-08-17 15:51:04 -0700

    Updated with new question

---
+++ src/main/java/WhileLoop1to50.java
@@ -0,0 +1,15 @@
+//print all even numbers between 1 and 50
+
+public class WhileLoop1to50
+{
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 50) {
+            {
+                System.out.println(i);
+                i += 2;
+            }
+
+        }
+    }
+}
