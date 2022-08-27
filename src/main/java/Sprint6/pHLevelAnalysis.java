ea6ebbccf9d2fad627bcbf2a11f80f7ee985900c
sujaybk2 <sjbhaskar2@gmail.com>
2022-08-26 20:40:23 -0700

    Update pHLevelAnalysis.java

--- src/main/java/Sprint6/pHLevelAnalysis.java
+++ src/main/java/Sprint6/pHLevelAnalysis.java
@@ -5,6 +5,18 @@ public class pHLevelAnalysis {
         float phValue;
         Scanner scannerObj = new Scanner(System.in);
         System.out.println("Enter the pH value: ");
+        phValue = scannerObj.nextFloat();
+        if (phValue<7)
+            {
+            return "The pH value is low, adjust water soon.";
+            }
+        else if (phValue >= 7 && phValue <=8)
+            {
+            return "The pH value is satisfactory.";
+            }
+        else {
+            return "The pH value is high, adjust water soon.";
+        }
 
     }
 }
